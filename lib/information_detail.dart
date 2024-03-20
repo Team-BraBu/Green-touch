d08f10a64689b2281d1f766cdbe484cccfabd372
JaeHyungL2 <wednestues18@gmail.com>
2024-03-20 16:08:55 +0900

    first commit -start

---
+++ lib/information_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class information_detail extends StatefulWidget {
+  const information_detail({super.key});
+
+  @override
+  State<information_detail> createState() => _information_detailState();
+}
+
+class _information_detailState extends State<information_detail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
