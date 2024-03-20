2325c245dbe780eb09ff103e158d07580ba2615b
thdrkfka <ssongjiaran@gmail.com>
2024-03-20 18:48:23 +0900

    [Feat] 식물 상품리스트 페이지로 이동 및 상품리스트 구현 세부사항 - 식물 이미지 images 파일에 옮김. 할일목록 - 상품리스트 image에 구현해야 한다.

--- lib/productlist.dart
+++ lib/productlist.dart
@@ -10,6 +10,21 @@ class ProductList extends StatefulWidget {
 class _ProductListState extends State<ProductList> {
   @override
   Widget build(BuildContext context) {
+    final plantItem = {
+      "list": [
+        {"image": "assets/images/plant1.png", "name": "아레카야자"},
+        {"image": "assets/images/plant2.png", "name": "관음죽"},
+        {"image": "assets/images/plant3.png", "name": "대나무야자"},
+        {"image": "assets/images/plant4.png", "name": "인도고무나무"},
+        {"image": "assets/images/plant5.jpg", "name": "아이비"},
+        {"image": "assets/images/plant6.png", "name": "황금죽"},
+        {"image": "assets/images/plant7.png", "name": "피닉스야자"},
+        {"image": "assets/images/plant8.png", "name": "보스턴고사리"},
+        {"image": "assets/images/plant9.png", "name": "스파티필름"},
+        {"image": "assets/images/plant10.png", "name": "행운목"},
+      ]
+    };
+
     return Scaffold(
         appBar: AppBar(
           actions: [
@@ -21,11 +36,27 @@ class _ProductListState extends State<ProductList> {
           mainAxisSpacing: 8,
           crossAxisSpacing: 8,
           padding: EdgeInsets.all(8),
-          children: List.generate(10, (index) {
-            return Center(
-              child: Text('item $index'),
-            );
-          }),
+          children: List.generate(
+            10,
+            (index) {
+              return Center(
+                child: ListView(
+                  children: <Widget>[
+                    Image.asset('assets/images/plant1.png'),
+                    // Image.asset('assets/images/plant2.png'),
+                    // Image.asset('assets/images/plant/plant3.png'),
+                    // Image.asset('assets/images/plant/plant4.png'),
+                    // Image.asset('assets/images/plant/plant5.jpg'),
+                    // Image.asset('assets/images/plant/plant6.png'),
+                    // Image.asset('assets/images/plant/plant7.png'),
+                    // Image.asset('assets/images/plant/plant8.png'),
+                    // Image.asset('assets/images/plant/plant9.png'),
+                    // Image.asset('assets/images/plant/plant10.png'),
+                  ],
+                ),
+              );
+            },
+          ),
         ));
   }
 }
--- lib/productlist_image.dart
+++ lib/productlist_image.dart
@@ -1,1 +1,33 @@
-class ProductListImage {}
+class ProductListImage {
+  final plantItem = {
+    "list": [
+      {"image": "assets/images/plant1.png", "name": "아레카야자"},
+      {"image": "assets/images/plant2.png", "name": "관음죽"},
+      {"image": "assets/images/plant3.png", "name": "대나무야자"},
+      {"image": "assets/images/plant4.png", "name": "인도고무나무"},
+      {"image": "assets/images/plant5.jpg", "name": "아이비"},
+      {"image": "assets/images/plant6.png", "name": "황금죽"},
+      {"image": "assets/images/plant7.png", "name": "피닉스야자"},
+      {"image": "assets/images/plant8.png", "name": "보스턴고사리"},
+      {"image": "assets/images/plant9.png", "name": "스파티필름"},
+      {"image": "assets/images/plant10.png", "name": "행운목"},
+    ]
+  };
+
+  List<String> productImages = [
+    'assets/images/plant1.png',
+    'assets/images/plant2.png',
+    'assets/images/plant3.png',
+    'assets/images/plant4.png',
+    'assets/images/plant5.jpg',
+    'assets/images/plant6.png',
+    'assets/images/plant7.png',
+    'assets/images/plant8.png',
+    'assets/images/plant9.png',
+    'assets/images/plant10.png',
+  ];
+
+  ProductListImage() {}
+
+  void getImage() {}
+}
