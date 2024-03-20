c39649918c07dba5d3c6c867efc0660f75a9f81c
JaeHyungL2 <wednestues18@gmail.com>
2024-03-20 16:46:42 +0900

    from here

--- lib/information_detail.dart
+++ lib/information_detail.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class information_detail extends StatefulWidget {
-  const information_detail({super.key});
+class information_detail extends StatelessWidget {
+  //const information_detail({super.key});
 
-  @override
-  State<information_detail> createState() => _information_detailState();
-}
-
-class _information_detailState extends State<information_detail> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '공기정화식물',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.indigo,
+
+        // body: GridView.count(
+        //   crossAxisCount: 2,
+        //   mainAxisSpacing: 8,
+        //   crossAxisSpacing: 8,
+        //   padding: EdgeInsets.all(8),
+        //   children: List.generate(10, (index) {}),
+        // ));
+      ),
+    );
   }
 }
