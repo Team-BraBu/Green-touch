5ad7b073b2f56f40b8f8cb15a1c49fc1bfbbb8e5
syong99 <sy99639@naver.com>
2024-03-21 14:33:48 +0900

    -상단 레이아웃 -장바구니 창 합침 -carousel_slider: ^4.0.0 패키지 추가

--- lib/layout/appbar.dart
+++ lib/layout/appbar.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../main_Page.dart';
+import '../tab_cart.dart';
 
 class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
@@ -9,33 +10,37 @@ class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MainPage()),
-          );
-        },
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/logo.png',
-              fit: BoxFit.contain,
-              height: 80,
-            ),
-          ],
+      leading: IconButton(
+        icon: Icon(
+          CupertinoIcons.bars,
+          color: Color(0xFF739072),
+          size: 40,
         ),
+        onPressed: () {
+          Navigator.pop(context);
+        },
       ),
       actions: [
         IconButton(
           icon: Icon(
             Icons.shopping_bag_outlined,
             color: Color(0xFF739072),
+            size: 35,
           ),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => tabcart()),
+            );
+          },
         ),
       ],
+      title: Image.asset(
+        'assets/images/logo.png',
+        fit: BoxFit.contain,
+        scale: 4.5,
+      ),
+      centerTitle: true,
     );
   }
 }
