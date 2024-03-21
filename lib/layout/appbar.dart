5ab07a4bde2411373abc7d8160dc93558654cd4d
ruah0807 <kimhk0315@gmail.com>
2024-03-21 21:10:44 +0900

    ## Description > 승용님 햄버거바 작업
    ## To-Do List
    - [x] 위젯 찾기 (search)
    - [x] Drawer 적용
    
    ## ETC.
    -

--- lib/layout/appbar.dart
+++ lib/layout/appbar.dart
@@ -1,41 +1,58 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:greentouch/main_Page.dart';
 
-import '../main_Page.dart';
+class BaseAppBar extends StatefulWidget implements PreferredSizeWidget {
+  @override
+  State<BaseAppBar> createState() => _BaseAppBarState();
 
-class BaseAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
 
+class _BaseAppBarState extends State<BaseAppBar> {
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      leading: IconButton(
-        icon: Icon(
-          CupertinoIcons.bars,
-          color: Color(0xFF739072),
-          size: 40,
-        ),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      ),
-      actions: [
-        IconButton(
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
           icon: Icon(
-            Icons.shopping_bag_outlined,
+            CupertinoIcons.bars,
             color: Color(0xFF739072),
-            size: 35,
+            size: 40,
           ),
-          onPressed: () {},
+          onPressed: () {
+            Scaffold.of(context)
+                .openDrawer(); // leading 아이콘 버튼을 눌렀을 때 Drawer를 열도록 함
+          },
         ),
-      ],
-      title: Image.asset(
-        'assets/images/logo.png',
-        fit: BoxFit.contain,
-        scale: 4.5,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.shopping_bag_outlined,
+              color: Color(0xFF739072),
+              size: 35,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        title: GestureDetector(
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MainPage(),
+              ),
+            );
+          },
+          child: Image.asset(
+            'assets/images/logo.png',
+            fit: BoxFit.contain,
+            scale: 4.5,
+          ),
+        ),
+        centerTitle: true,
       ),
-      centerTitle: true,
     );
   }
 }
