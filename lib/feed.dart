8f9b9a3b5e5e5f2ef6f4a8ff38554974cca00c69
thdrkfka <ssongjiaran@gmail.com>
2024-03-21 11:36:48 +0900

    [Feat] 플랜테리어 페이지의 피드 형식 세부사항 - 피드 한 개 제작 - 리스트 처럼 보이도록 할 예정

---
+++ lib/feed.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Feed extends StatefulWidget {
+  const Feed({super.key});
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  bool isFavorite = false;
+  bool isMarked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Image.asset(
+            'assets/images/planterior1.png',
+            height: 400,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  isFavorite = !isFavorite;
+                });
+              },
+              icon: Icon(CupertinoIcons.heart),
+              color: isFavorite ? Colors.red : Colors.black,
+            ),
+            Spacer(),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    isMarked = !isMarked;
+                  });
+                },
+                icon: Icon(CupertinoIcons.bookmark, color: ,))
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text('플랜테리어 짱'),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Text('#플랜테리어 #식물 #초보자가_키우기_쉬운_식물'),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Text(
+            '2024년 03월 21일',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+      ],
+    );
+  }
+}
