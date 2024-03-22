aa23648ff1bcce759e09b73f9fa2c0d0d2584388
thdrkfka <ssongjiaran@gmail.com>
2024-03-22 20:50:04 +0900

    [Fix] 플랜테리어 북마크 아이콘 누를 시, 활동내역에 저장 세부사항 - 진행 중

--- lib/savedfeedpage.dart
+++ lib/savedfeedpage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:greentouch/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SavedFeedPage extends StatefulWidget {
