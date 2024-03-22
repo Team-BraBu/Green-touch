44a124ee803d0e3215bf9551a3dbeb2d48301946
jjunnong <cacao3216@gmail.com>
2024-03-22 16:08:45 +0900

    회원가입,로그인 구현

--- lib/user/login_Page.dart
+++ lib/user/login_Page.dart
@@ -1,9 +1,6 @@
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:greentouch/layout/appbar.dart';
-import 'package:greentouch/main_Page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:greentouch/main_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -13,84 +10,102 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: BaseAppBar(),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '로그인',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.normal,
-                  color: Color(0xFF3A4D39),
-                  fontFamily: 'Jua',
-                ),
+      appBar: AppBar(
+        title: Text('로그인'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '로그인',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.normal,
+                color: Color(0xFF3A4D39),
+                fontFamily: 'Jua',
               ),
-              SizedBox(
-                height: 20,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: '아이디',
               ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      color: Color(0xFF739072),
-                    ),
-                  ),
-                  hintText: '아이디를 입력해주세요.',
-                  hintStyle: TextStyle(
-                    color: Color(0xFF739072),
-                  ),
-                ),
+            ),
+            SizedBox(height: 30),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: '비밀번호',
               ),
-              SizedBox(height: 30),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: '비밀번호를 입력해주세요.',
-                  hintStyle: TextStyle(
-                    color: Color(0xFF739072),
-                  ),
-                ),
-              ),
-              SizedBox(height: 25),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MainPage()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF739072),
-                  fixedSize: Size(380, 60),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                child: Text(
-                  '로그인',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.white,
-                    fontFamily: 'Jua',
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+            SizedBox(height: 25),
+            ElevatedButton(
+              onPressed: () {
+                _login();
+              },
+              child: Text('로그인'),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  void _login() async {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    if (username.isEmpty || password.isEmpty) {
+      _showAlert('경고', '아이디와 비밀번호를 입력해주세요.');
+      return;
+    }
+
+    // SharedPreferences에서 회원 가입 정보 가져오기
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? savedUsername = prefs.getString('username');
+    String? savedPassword = prefs.getString('password');
+
+    // 입력한 아이디와 비밀번호와 저장된 정보 비교
+    if (username == savedUsername && password == savedPassword) {
+      // 로그인 성공 시 메인 페이지로 이동
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MainPage()),
+      );
+    } else {
+      _showAlert('경고', '아이디 또는 비밀번호가 일치하지 않습니다.');
+    }
+  }
+
+  void _showAlert(String title, String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('확인'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/user/register_Page.dart
+++ lib/user/register_Page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:greentouch/layout/app_drawer.dart';
 import 'package:greentouch/layout/appbar.dart';
 import 'package:greentouch/main_Page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -14,103 +15,105 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BaseAppBar(),
       drawer: AppDrawer(),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: '아이디',
+              ),
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: '비밀번호',
+              ),
+              obscureText: true,
+            ),
+            TextField(
+              controller: _confirmPasswordController,
+              decoration: InputDecoration(
+                labelText: '비밀번호 확인',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _register();
+              },
+              child: Text(
                 '회원가입',
                 style: TextStyle(
-                  fontSize: 30,
+                  fontSize: 20,
                   fontWeight: FontWeight.normal,
-                  color: Color(0xFF3A4D39),
+                  color: Colors.white,
                   fontFamily: 'Jua',
                 ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color(0xFF739072),
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: '아이디를 입력해주세요.',
-                  hintStyle: TextStyle(
-                    color: Color(0xFF739072),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color(0xFF739072),
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: '비밀번호를 입력해주세요.',
-                  hintStyle: TextStyle(
-                    color: Color(0xFF739072),
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: '비밀번호를 다시 입력해주세요.',
-                  hintStyle: TextStyle(
-                    color: Color(0xFF739072),
-                  ),
-                ),
-              ),
-              SizedBox(height: 25),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MainPage()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF739072),
-                  fixedSize: Size(380, 60),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                child: Text(
-                  '회원가입',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.white,
-                    fontFamily: 'Jua',
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  void _register() async {
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+    String confirmPassword = _confirmPasswordController.text;
+
+    if (username.isEmpty || password.isEmpty || confirmPassword.isEmpty) {
+      _showAlert('경고', '모든 필수 정보를 입력해주세요.');
+      return;
+    }
+
+    if (password != confirmPassword) {
+      _showAlert('경고', '비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+      return;
+    }
+
+    // 회원가입 정보를 SharedPreferences에 저장
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('username', username);
+    await prefs.setString('password', password);
+
+    // 회원가입 성공시 메인 페이지로 이동
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MainPage()),
+    );
+  }
+
+  void _showAlert(String title, String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('확인'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
