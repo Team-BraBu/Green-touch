87a41616988243a83466026e6ca07955d9e218fb
JaeHyungL2 <wednestues18@gmail.com>
2024-03-27 17:40:48 +0900

    휴 오늘의미션과 활동내역 연동완료.

--- lib/list/history.dart
+++ lib/list/history.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:greentouch/layout/appbar_back.dart';
+import 'package:provider/provider.dart';
+
+import '../service/activity_service.dart'; // ActivityModel 클래스 정의 파일
 
 class HistoryPage extends StatelessWidget {
-  final List<Activity> activities = [
-    Activity(
-      date: '2023-03-27',
-      description: '햇빛 채우기 작업을 완료했습니다.',
-      iconData: Icons.wb_sunny,
-    ),
-    Activity(
-      date: '2023-03-26',
-      description: '물주기 작업을 완료했습니다.',
-      iconData: Icons.invert_colors,
-    ),
-    // 추가 활동 내역...
-  ];
-
   @override
   Widget build(BuildContext context) {
+    final activities = Provider.of<ActivityModel>(context).activities;
+
     return Scaffold(
       appBar: BackAppbar(),
       body: ListView.builder(
@@ -39,12 +30,43 @@ class HistoryPage extends StatelessWidget {
     );
   }
 }
-
-class Activity {
-  String date;
-  String description;
-  IconData iconData;
-
-  Activity(
-      {required this.date, required this.description, required this.iconData});
-}
+//   @override
+//   void initState() {
+//     super.initState();
+//     if (widget.initialActivities != null) {
+//       activities.addAll(widget.initialActivities!);
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: BackAppbar(),
+//       body: ListView.builder(
+//         itemCount: activities.length,
+//         itemBuilder: (context, index) {
+//           return Card(
+//             margin: EdgeInsets.all(8),
+//             child: ListTile(
+//               leading: Icon(
+//                 activities[index].iconData,
+//                 color: Theme.of(context).primaryColor,
+//               ),
+//               title: Text(activities[index].date),
+//               subtitle: Text(activities[index].description),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+//
+// class Activity {
+//   String date;
+//   String description;
+//   IconData iconData;
+//
+//   Activity(
+//       {required this.date, required this.description, required this.iconData});
+// }
--- lib/list/mission.dart
+++ lib/list/mission.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:greentouch/layout/appbar_back.dart';
+import 'package:intl/intl.dart'; // DateFormat를 포함한 intl 패키지를 임포트합니다.
+import 'package:provider/provider.dart';
+
+import '../service/activity_service.dart';
+import 'history.dart';
 
 class MissionPage extends StatefulWidget {
   @override
@@ -21,6 +26,20 @@ class _MissionPageState extends State<MissionPage> {
         description: "아이들의 상태를 기록해주세요.",
         iconData: Icons.book),
   ];
+  //미션연동위한 코드 ↓
+  void completeMission(int index, bool? isCompleted) {
+    if (isCompleted ?? false) {
+      final activity = Activity(
+        date: DateFormat('yyyy-MM-dd').format(DateTime.now()),
+        description: missions[index].description,
+        iconData: missions[index].iconData,
+      );
+      // Provider를 사용하여 ActivityModel에 접근하고 활동을 추가합니다.
+      Provider.of<ActivityModel>(context, listen: false).addActivity(activity);
+    }
+  }
+
+  // 여기서 HistoryPage 생성자에 activity를 전달하려면 HistoryPage도 수정 필요
 
   @override
   Widget build(BuildContext context) {
@@ -46,13 +65,11 @@ class _MissionPageState extends State<MissionPage> {
                 itemCount: missions.length,
                 itemBuilder: (context, index) {
                   return MissionTile(
-                    mission: missions[index],
-                    onChanged: (bool? value) {
-                      setState(() {
-                        missions[index].isCompleted = value ?? false;
+                      mission: missions[index],
+                      onChanged: (bool? value) {
+                        completeMission(
+                            index, value); // 수정: 여기서 completeMission 호출
                       });
-                    },
-                  );
                 },
               ),
             ),
@@ -62,6 +79,8 @@ class _MissionPageState extends State<MissionPage> {
     );
   }
 }
+
+//미션완료시 activity생성. 새로추가
 
 class MissionTile extends StatelessWidget {
   final Mission mission;
