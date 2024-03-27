cdd92584a0d3b2eff19e3525334553c5b0591cf9
syong99 <sy99639@naver.com>
2024-03-27 17:27:05 +0900

    구매창 수정

--- lib/mypage/purchase.dart
+++ lib/mypage/purchase.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:greentouch/layout/appbar_back.dart';
 import 'package:greentouch/list/purchase_complete.dart';
+import 'package:greentouch/mypage/product_reviews.dart';
 import 'package:greentouch/service/cart_service.dart';
 import 'package:provider/provider.dart';
 
@@ -15,10 +17,14 @@ class PurchasePage extends StatefulWidget {
 
 class _PurchasePageState extends State<PurchasePage> {
   final TextEditingController addressController = TextEditingController();
-
   final TextEditingController cardNumberController = TextEditingController();
   final TextEditingController expiryDateController = TextEditingController();
   final TextEditingController cvvController = TextEditingController();
+
+  void showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,15 +35,16 @@ class _PurchasePageState extends State<PurchasePage> {
         totalPrice += product[i].price;
       }
       return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: BackAppbar(),
-        body: Builder(builder: (context) {
-          return Padding(
+        body: SingleChildScrollView(
+          child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
-                  '구매물품',
+                  '구매물품\t${product.length}개',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.normal,
@@ -48,27 +55,23 @@ class _PurchasePageState extends State<PurchasePage> {
                 SizedBox(
                   height: 20,
                 ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: product.length,
-                    itemBuilder: (context, index) {
-                      Plant products = product[index];
-                      return ListTile(
-                        leading: Image.asset(
-                          products.imagePath,
-                          width: 50,
-                          height: 50,
-                          fit: BoxFit.cover,
-                        ),
-                        title: Text(products.title),
-                        subtitle: Text('${products.price} 원'),
-                      );
-                    },
-                  ),
-                ),
-                ListTile(
-                  title: Text('총 가격'),
-                  subtitle: Text('${totalPrice.toString()} 원'),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: product.length,
+                  itemBuilder: (context, index) {
+                    Plant products = product[index];
+                    return ListTile(
+                      leading: Image.asset(
+                        products.imagePath,
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover,
+                      ),
+                      title: Text(products.title),
+                      subtitle: Text('${products.price} 원'),
+                    );
+                  },
                 ),
                 SizedBox(
                   height: 30,
@@ -105,6 +108,7 @@ class _PurchasePageState extends State<PurchasePage> {
                     hintText: '카드 번호를 입력해주세요.',
                     border: OutlineInputBorder(),
                   ),
+                  keyboardType: TextInputType.number,
                 ),
                 SizedBox(height: 10),
                 Row(
@@ -117,51 +121,102 @@ class _PurchasePageState extends State<PurchasePage> {
                           hintText: '만료일 (MM/YY)',
                           border: OutlineInputBorder(),
                         ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(4),
+                        ],
                       ),
                     ),
                     SizedBox(width: 10),
                     Expanded(
                       flex: 1,
                       child: TextField(
+                        obscureText: true,
                         controller: cvvController,
                         decoration: InputDecoration(
                           hintText: 'CVV',
                           border: OutlineInputBorder(),
                         ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(3),
+                        ],
                       ),
                     ),
                   ],
                 ),
                 SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF739072),
-                    fixedSize: Size(180, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xffece3ce),
+                        fixedSize: Size(180, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        context.read<CartService>().removeCartItems();
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        '구매취소',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color(0xff3a4d39),
+                          fontFamily: 'Jua',
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
                     ),
-                  ),
-                  onPressed: () {
-                    context.read<CartService>().removeCartItems();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => PurchaseComplete()),
-                    );
-                  },
-                  child: Text(
-                    '결제하기',
-                    style: TextStyle(
-                        fontSize: 22,
-                        color: Colors.white,
-                        fontFamily: 'Jua',
-                        fontWeight: FontWeight.normal),
-                  ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF739072),
+                        fixedSize: Size(180, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (addressController.text.isEmpty) {
+                          showSnackBar(context, '주소를 입력해주세요.');
+                        } else if (cardNumberController.text.length != 16) {
+                          showSnackBar(context, '올바른 카드번호가 아닙니다.');
+                        } else if (expiryDateController.text.isEmpty) {
+                          showSnackBar(context, '만료일을 입력해주세요.');
+                        } else if (cvvController.text.isEmpty) {
+                          showSnackBar(context, 'cvv번호를 입력해주세요.');
+                        } else {
+                          context.read<CartService>().removeCartItems();
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PurchaseComplete(),
+                              settings: RouteSettings(
+                                arguments: ProductReview(), // 여기에 데이터를 전달합니다.
+                              ),
+                            ),
+                          );
+                        }
+                      },
+                      child: Text(
+                        '${totalPrice.toString()} 원 결제하기',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                          fontFamily: 'Jua',
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          );
-        }),
+          ),
+        ),
       );
     });
   }
