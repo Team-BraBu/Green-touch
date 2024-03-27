7772f66524d0f04050a3e13d3dd3e4ee79b60de9
thdrkfka <ssongjiaran@gmail.com>
2024-03-27 13:28:12 +0900

    [Fix] 아이콘 상태 변화 기능 파일 구분

--- lib/list/feed.dart
+++ lib/list/feed.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'feediconstate.dart';
+
 class Feed extends StatefulWidget {
   const Feed({
     Key? key,
@@ -132,35 +134,5 @@ class _FeedState extends State<Feed> {
         )
       ],
     );
-  }
-}
-
-class FeedIconState {
-  final SharedPreferences prefs;
-
-  FeedIconState(this.prefs);
-
-  Future<void> saveBookmarkStatus(String imagePath, bool isMarked) async {
-    await prefs.setBool(imagePath, isMarked);
-  }
-
-  bool loadBookmarkStatus(String imagePath) {
-    return prefs.getBool(imagePath) ?? false;
-  }
-
-  Future<void> saveFavoriteStatus(String imagePath, bool isFavorite) async {
-    await prefs.setBool('favorite_$imagePath', isFavorite);
-  }
-
-  bool loadFavoriteStatus(String imagePath) {
-    return prefs.getBool('favorite_$imagePath') ?? false;
-  }
-
-  Future<void> saveLikesCount(String imagePath, int likesCount) async {
-    await prefs.setInt('likes_$imagePath', likesCount);
-  }
-
-  int loadLikesCount(String imagePath) {
-    return prefs.getInt('likes_$imagePath') ?? 356;
   }
 }
