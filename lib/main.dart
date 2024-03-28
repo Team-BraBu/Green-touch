c29d37d3b456b1d859aa2a7e1567f408da719691
Ruah Kim <kimhk0315@gmail.com>
2024-03-28 17:03:59 +0900

    메인화면 미출력 오류 해결
    중복코드 제거, 안쓰는 페이지 제거

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,8 @@ late SharedPreferences prefs;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  //스플레쉬 위젯 적용
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   prefs = await SharedPreferences.getInstance();
+  FlutterNativeSplash.remove();
 
   runApp(
     //서비스 등록
@@ -57,42 +55,5 @@ class MyApp extends StatelessWidget {
       title: 'Your App Title',
       home: isOnboarded ? MainPage() : OnBoarding(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // 스플레쉬 적용 initState
-  @override
-  void initState() {
-    super.initState();
-    initialization();
-  }
-
-  // 스플레쉬 적용 initialization
-  void initialization() async {
-    // This is where you can initialize the resources needed by your app while
-    // the splash screen is displayed.  Remove the following example because
-    // delaying the user experience is a bad design practice!
-    // ignore_for_file: avoid_print
-    print('ready in 3...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('ready in 2...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('ready in 1...');
-    await Future.delayed(const Duration(seconds: 1));
-    print('go!');
-    FlutterNativeSplash.remove();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return OnBoarding();
   }
 }
