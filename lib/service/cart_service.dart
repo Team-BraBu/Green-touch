dcb94d8d9595240bcc9465c237ea87afb6dc8d1f
jjunnong <cacao3216@gmail.com>
2024-03-28 18:15:52 +0900

    최종본2

--- lib/service/cart_service.dart
+++ lib/service/cart_service.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:greentouch/service/review_service.dart';
 import 'package:uuid/uuid.dart';
 
+import '../mypage/tab_cart.dart';
 import '../product/plant_service.dart';
 
 class CartService extends ChangeNotifier {
   List<Plant> product = [];
   List<Plant> get plant => product;
+  List<Product> _items = []; // 장바구니 아이템 목록
+  List<Product> get items => _items;
 
   //ReviewService 인스턴스 주입
   final ReviewService reviewService;
@@ -15,8 +18,29 @@ class CartService extends ChangeNotifier {
   CartService(this.reviewService);
 
   void addToCart(Plant plant) {
-    product.add(plant);
-    notifyListeners();
+    // 이미 장바구니에 있는지 확인
+    bool alreadyInCart = product.contains(plant);
+
+    if (alreadyInCart) {
+      // 이미 장바구니에 있는 경우 수량을 증가시킴
+      for (Plant p in product) {
+        if (p == plant) {
+          p.quantity++; // 수량 증가
+          break;
+        }
+      }
+    } else {
+      // 장바구니에 없는 경우에는 새로 추가
+      plant.quantity = 1; // 수량 초기화
+      product.add(plant); // 상품 추가
+    }
+
+    notifyListeners(); // 상태 변경 알림
+  }
+
+  void addItem(Product product) {
+    _items.add(product); // 아이템을 장바구니에 추가
+    notifyListeners(); // 리스너에게 변경을 알림
   }
 
   void Function(List<Plant>)? onDataTransferred;
